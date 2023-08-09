efa0946d7d70423cae848ba193847eb38d7bfd57
Marcinho Coutinho <marciojscoutinho@gmail.com>
2023-08-09 16:11:17 -0300

    feat: add update

--- src/pages/BookDetail/BookDetail.tsx
+++ src/pages/BookDetail/BookDetail.tsx
@@ -1,3 +1,25 @@
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+export interface BookState {
+  id: string
+  volumeInfo: {
+    title: string
+    subtitle: string
+    description: string
+    imageLinks?: {
+      thumbnail: string
+    }
+  }
+}
+
 export function BookDetail() {
+  const [book, setBook] = useState<BookState | null>(null)
+  const params = useParams()
+
+  const { bookId } = params
+
+  console.log({ bookId })
+
   return <h1>Detalhes do Livro</h1>
 }
--- src/pages/Books/Books.tsx
+++ src/pages/Books/Books.tsx
@@ -1,3 +1,60 @@
+import { useEffect, useState } from 'react'
+
+import { Link, Navigate, useLocation, useSearchParams } from 'react-router-dom'
+
+import { BookState as Book } from '../BookDetail'
+
+import { googleBooksApi } from '../../services/googleBooksApi'
+
+import { Thumbnail } from '../../components/Thumbnail/Thumbnail'
+
+import { Container, Title, SubTitle } from './Books.styles'
+
+interface BooksState {
+  totalItems: number
+  items: Book[]
+}
+
 export function Books() {
-  return <h1>Resultados da busca</h1>
+  const [books, setBooks] = useState<BooksState | null>(null)
+  const params = useSearchParams()
+  const location = useLocation()
+
+  const [searchParams] = params
+  const q = searchParams.get('q')
+
+  useEffect(() => {
+    googleBooksApi
+      .get(`/v1/volumes?q=${q}&maxResults=20`)
+      .then((response) => setBooks(response.data))
+  }, [q])
+
+  if (!q) {
+    return <Navigate to="/" state={{ from: location }} replace />
+  }
+
+  return (
+    <Container>
+      <h1>Resultado da sua busca</h1>
+
+      {books && (
+        <ul>
+          {books.items.map((book) => (
+            <li key={book.id}>
+              <Link to={'/Books/${book.id}'}>
+                <Thumbnail
+                  thumbnail={book.volumeInfo.imageLinks?.thumbnail}
+                  title={book.volumeInfo.title}
+                  bgColor="#d9d9d9"
+                />
+
+                <Title>{book.volumeInfo.title}</Title>
+                <SubTitle>{book.volumeInfo.subtitle}</SubTitle>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </Container>
+  )
 }
